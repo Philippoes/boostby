7e8f88958c48e2002969afb84bb21fb4baefda5c
martinlinder <martin.liiinder@gmail.com>
2017-11-10 20:59:34 +0100

    Create activity (#2)
    * Adds initial grid
    
    * Finish initial draft
    
    * Adds object used in view
    
    * Adds distance to home activity card.
    
    * Added functionality to create activities, and also show the most recently added activity
    
    * Fixed requested changes to create_activity

---
+++ src/pages/add-activity/add-activity.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+@IonicPage()
+@Component({
+  selector: 'page-add-activity',
+  templateUrl: 'add-activity.html',
+})
+export class AddActivityPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private storage: Storage) {
+  }
+
+  activity: {
+    image: string,
+    title: string,
+    info: string,
+    distance: number
+  };
+
+  save(title, info) {
+
+    this.activity = {
+      image: "http://lorempixel.com/400/300",
+      title: title,
+      info: info,
+      distance: 5
+    }
+
+    this.storage.get('activities').then((tempArray) => {
+      if (tempArray != null) {
+        tempArray.push(this.activity);
+        console.log(tempArray);
+        this.storage.set('activities', tempArray); 
+        this.showAlert();
+      } else {
+        let activityArray = [];
+
+        activityArray.push(this.activity); 
+        this.storage.set('activities', activityArray);
+        this.showAlert();
+      }
+
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+    this.navCtrl.pop();
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Activity created!',
+      subTitle: '',
+      buttons: ['OK']
+    }); 
+    
+    alert.present();
+  }
+}
